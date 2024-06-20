adb733b57c5371f6a9632918836b7ac3ebb93361
Abh7ay <abhay00991@gmail.com>
2024-06-20 16:03:00 +0530

    commited

--- api/serializers.py
+++ api/serializers.py
@@ -4,10 +4,11 @@ from .models import User, File
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'is_operation_user', 'is_client_user']
+        fields = ['id', 'username', 'password', 'is_ops_user', 'is_client_user']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        # sourcery skip: inline-immediately-returned-variable
         user = User.objects.create_user(**validated_data)
         return user
 
