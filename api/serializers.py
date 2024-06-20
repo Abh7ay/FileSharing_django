62dd64014c6cd0758b5fb787e050a24f036705e1
Abh7ay <abhay00991@gmail.com>
2024-06-20 15:59:34 +0530

    tera msg

---
+++ api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User, File
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'is_operation_user', 'is_client_user']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ['id', 'user', 'file', 'uploaded_at']
