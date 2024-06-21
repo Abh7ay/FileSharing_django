66cf137ea7867ff36171801cad9eaaecd4f70bc6
Abh7ay <abhay00991@gmail.com>
2024-06-21 13:34:23 +0530

    commit

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     is_ops_user = models.BooleanField(default=False)
     is_client_user = models.BooleanField(default=False)
+    verification_code = models.CharField(max_length=100, null=True, blank=True)
 
 class UploadedFile(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
